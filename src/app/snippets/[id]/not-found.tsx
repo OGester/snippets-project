b1fa9743906e81db6639bc6c35d87b2da8c3170f
Oskar Gester <gester.oskar@gmail.com>
2025-07-21 10:55:04 +0200

    dynamic path for rendering specific snippets created, initail custom 404 page also created

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl-bold">
+        Sorry, This particular snippet couldn't be found!
+      </h1>
+    </div>
+  );
+}
